a3dfc3547e7789b6cad808fd5294ef49dabac087
eatspaint <cef2010@gmail.com>
2018-04-23 15:45:07 -0700

    1.1.0

--- pkdir.rb
+++ pkdir.rb
@@ -1,9 +1,9 @@
 class Pkdir < Formula
   desc ""
   homepage "https://github.com/eatspaint/pkdir"
-  url "https://github.com/eatspaint/pkdir/archive/1.0.4.tar.gz"
-  version "1.0.4"
-  sha256 "b6e187160e8c5e06b92d816cc5d31c72bfe98f0750bad2453e4195df64fd8381"
+  url "https://github.com/eatspaint/pkdir/archive/1.1.0.tar.gz"
+  version "1.1.0"
+  sha256 ""
 
 
   def install
